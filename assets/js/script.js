d0dead1dcae738263c3fe68ca863045cd93de848
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-02-16 14:49:04 -0300

    projeto kenzie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,62 @@
-const tasks = [
-  {title: "Comprar comida para o gato", type: "Urgente"},
-  {title: "Consertar Computador", type: "Importante"},
-  {title: "Beber água", type: "Normal"},
-  {title: "Enviar relatório trimestral", type: "Importante"},
-  {title: "Fazer exercícios físicos", type: "Normal"},
-  {title: "Agendar consulta médica", type: "Urgente"},
-  {title: "Ler pelo menos um capítulo de um livro", type: "Normal"},
-  {title: "Limpar a despensa", type: "Importante"},
-  {title: "Pagar a conta de energia", type: "Urgente"},
-  {title: "Assistir a um documentário interessante", type: "Normal"},
-];
+const sendBtn = document.querySelector('.form__button--add-task')
+const taskList = document.querySelector('.tasks__list')
+const txtInput = document.querySelector('.form__input--text')
+const selectInput = document.querySelector('.form__input--priority')
+
+const createElement = (tag, className, callback) => {
+  const element = document.createElement(tag)
+  element.className = (className)
+  return element
+}
+
+const checkTaskTipe = (tipe) => {
+  let element = ''
+
+  if (tipe == 'normal') {
+    return element = createElement('span', 'task-type span-normal')
+  }
+
+  if (tipe == 'importante') {
+    return element = createElement('span', 'task-type span-important')
+  }
+
+  if (tipe == 'urgente') {
+    return element = createElement('span', 'task-type span-urgent')
+  }
+
+}
+
+const createTask = (name, tipe) => {
+
+  const taskItem = createElement('li', 'task__item')
+  const taskInfo = createElement('div', 'task-info__container')
+  const taskTipe = checkTaskTipe(tipe)
+  const taskName = document.createElement('p')
+  taskName.innerHTML = name
+
+  const removeTaskBtn = createElement('button', 'task__button--remove-task', )
+
+  removeTaskBtn.addEventListener('click', (e)=> {
+    e.preventDefault
+    taskItem.remove()
+  } )
+
+  taskItem.appendChild(taskInfo)
+  taskItem.appendChild(removeTaskBtn)
+  taskInfo.appendChild(taskTipe)
+  taskInfo.appendChild(taskName)
+
+
+  return taskItem
+
+}
+
+sendBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  const name = txtInput.value
+  const tipe = selectInput.value
+  createTask(name, tipe)
+
+  taskList.appendChild(createTask(name, tipe))
+})
